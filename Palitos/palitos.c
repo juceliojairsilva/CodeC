aafad07d84d27c11d584c437943875c5bf82474e
Jucelio Jair Silva <juceliojairsilva@gmail.com>
2013-12-10 01:32:30 -0200

    Commit inicial

---
+++ Palitos/palitos.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+
+	// Para começar o jogo todos tem 3 palitos em suas mãos.
+    int palitoJogador1 = 3;
+    int palitoJogador2 = 3;
+    int palitoJogador3 = 3;
+    int palitoJogador4 = 3;
+    int rodada = 0;
+
+    int palitosMao1;
+    int palitosMao2;
+    int palitosMao3;
+    int palitosMao4;
+
+    int Jogador1;
+    int Jogador2;
+    int Jogador3;
+    int Jogador4;
+
+    int somaMao;
+
+    int escolhaJogador1;
+    int escolhaJogador2;
+    int escolhaJogador3;
+    int escolhaJogador4;
+
+
+
+    srand( (unsigned)time(NULL) );
+ 
+    // Loop para o jogo rodar.
+ 	while( (palitoJogador1 != 0) || (palitoJogador2 != 0) || (palitoJogador3 != 0) || (palitoJogador4 != 0) ) {
+
+            printf("************************************************************ \n\n");
+
+ 			printf("Rodada : %d \n\n", rodada);
+            printf("Qunatidade de Palitos\n");
+            printf("Jogador1: %d \n ",palitoJogador1);
+            printf("Jogador2: %d \n ",palitoJogador2);
+            printf("Jogador3: %d \n ",palitoJogador3);
+            printf("Jogador4: %d \n \n",palitoJogador4);
+
+ 			if(rodada == 0) {
+	 			palitosMao1 = rand() % palitoJogador1 + 1;
+	 		    sleep(1);
+
+	 		    palitosMao2 = rand() % palitoJogador2 + 1;
+	 		    sleep(1);
+
+	 		    int palitosMao3 = rand() % palitoJogador3 + 1;
+	 		    sleep(1);
+
+	 		    palitosMao4 = rand() % palitoJogador4 + 1;
+	 		    sleep(1);
+
+	 		    rodada = rodada + 1;
+ 			}else {
+
+     			//Escolha dos palitos que vão para mesa.
+     		    palitosMao1 = 1 + rand() % palitoJogador1;
+     		    sleep(1);
+
+     		    palitosMao2 = 1 + rand() % palitoJogador2;
+     		    sleep(1);
+
+     		    palitosMao3 = 1 + rand() % palitoJogador3;
+     		    sleep(1);
+
+     		    palitosMao4 = 1 + rand() % palitoJogador4;
+     		    sleep(1);
+            }
+ 		    
+ 		    printf("Escolha dos numeros pelos Jogadores \n");
+
+ 		    escolhaJogador1 = 1 + rand() % (palitoJogador1 + palitoJogador2 + palitoJogador3 + palitoJogador4);
+ 		    sleep(1);
+ 		    printf("Jogador1 : %d\n", escolhaJogador1);
+
+ 		    escolhaJogador2 = 1 + rand() % (palitoJogador1 + palitoJogador2 + palitoJogador3 + palitoJogador4);
+ 		    sleep(1);
+ 		    printf("Jogador2 : %d\n", escolhaJogador2);
+
+ 		    escolhaJogador3 = 1 + rand() % (palitoJogador1 + palitoJogador2 + palitoJogador3 + palitoJogador4);
+ 		    sleep(1);
+ 		    printf("Jogador3 : %d\n", escolhaJogador3);
+
+ 		    escolhaJogador4 = 1 + rand() % (palitoJogador1 + palitoJogador2 + palitoJogador3 + palitoJogador4);
+ 		    sleep(1);
+ 		    printf("Jogador4 : %d \n \n", escolhaJogador4);
+
+ 		    printf("Quantos Palitos na Mão \n");
+ 		    printf("Jogador1 : %d \n",palitosMao1);
+ 		    printf("Jogador2 : %d \n",palitosMao2);
+ 		    printf("Jogador3 : %d \n",palitosMao3);
+ 		    printf("Jogador4 : %d \n \n",palitosMao4);
+
+ 		    somaMao = (palitosMao1 + palitosMao2 + palitosMao3 + palitosMao4);
+ 		    printf("Soma dos Palitos: %d \n\n", somaMao);
+
+ 		    if (escolhaJogador1 == somaMao) {
+
+ 		    	palitoJogador1 = palitoJogador1 - 1;
+ 		    	printf("Jogador1  Acertou \n \n");
+                printf("Numero de Palitos %d \n \n", palitoJogador1);
+
+ 		    }else if(escolhaJogador2 == somaMao){
+
+ 		    	palitoJogador2 = palitoJogador2 - 1;
+ 		    	printf("Jogador2  Acertou \n \n");
+                printf("Numero de Palitos %d \n \n", palitoJogador2);
+
+ 		    }else if(escolhaJogador3 == somaMao){
+
+ 		    	palitoJogador3 = palitoJogador3 - 1;
+ 		    	printf("Jogador3  Acertou \n \n");
+                printf("Numero de Palitos %d \n \n", palitoJogador3);
+
+ 		    }else if(escolhaJogador4 == somaMao){
+
+ 		    	palitoJogador4 = palitoJogador4 - 1;
+ 		    	printf("Jogador4  Acertou \n \n");
+                printf("Numero de Palitos %d \n \n", palitoJogador4);
+
+ 		    }else {
+
+ 		    	printf("Ninguem Acertou \n \n");
+ 		    }
+
+ 		    rodada = rodada + 1;
+
+ 	}
+
+    printf("Parabéns \n \n");
+    	
+}
---
+++ Palitos/readme.txt
@@ -0,0 +1,22 @@
+
+
+	Palitos.c
+
+
+	Estava eu final de semana com amigos, após um churrasco resolvemos jogar alguns
+	jogos de familia, então alguem deu a ideia de jogarmos o jogo dos palitos.
+
+
+
+	Lógica do jogo:
+
+		Temos 4 jogadores(pode ter mais), cada jogador fica com 3 palitos, com as mãos escondidas ele escolhe
+		de 0 a 3 a quantidade de palitos que irá colocar em sua outra mão fechada em cima da mesa, o mesmo acontece
+		para os demais participantes, só na primeira rodada é permitido ir com 0 para a mesa, então cada jogador
+		irá dizer um numero, que tem que ser de 0 a 12 já que temos 4 jogadores e cada um com 3 palitos 4x3=12
+		quando todos tiverem escolhido um numero os jogadores abrem a mão e soman os palitos que estão em todas as mãos
+		o jogador que acertou a quantidade de numeros, retira um palito que sua mão e coloca na mesa ficando com 2 palitos.
+		apartir da segunda rodada, não pode mas ir com 0 palitos para a mesa, o primeiro jogador que ficar com 0 palitos
+		na mão ganha o jogo.
+
+	Quando terminanos de jogar um comentário surgio, já penso fazer o computador jogar este jogo, bem então foi isto.
